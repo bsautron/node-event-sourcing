d7e5e19db1ba528537c72b1989607f30cc5ffcd2
bsautron <sautron.brunojj@gmail.com>
2017-03-25 18:07:05 +0100

    mini

--- index.js
+++ index.js
@@ -32,29 +32,10 @@ class EventSources {
 class Trader extends EventSources {
 	constructor() {
 		super('trader', {
-			coins: {
-				value: 0,
-				reduce: (prev, value) => prev + value
-			},
-			temp: {
-				value: 10,
-				reduce: (prev, value) => value
-			}
+			'coins': { value: 0, reduce: (prev, value) => prev + value },
+			'temp': { value: 10 }
 		});
-	}
-
-	add(data) {
-		super.add(data);
 	}
 }
 
-
 const trader = new Trader();
-
-trader.add({coins: 4});
-trader.add({coins: -34});
-trader.add({coins: 23});
-trader.add({temp: -1});
-const fig = trader.retrieve();
-console.log('fig:', fig);
-// console.log(trader.collection);
